a50aee57c191f22559820ece1cd8d4dc197f883e
Josh Harvey <josh@lamassu.is>
2016-04-22 19:30:33 +0300

    working

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ var OPTIONS = {
 
 var connection = new SMTPConnection(OPTIONS)
 var configuration
-var logger
 
-exports.config = function config (_config, _logger) {
-  logger = _logger
+exports.NAME = 'SMTP2GO'
+exports.SUPPORTED_MODULES = []
+
+exports.config = function config (_config) {
   configuration = _config
 }
 
-exports.sendMessage = function sendMessage (rec, cb) {
+exports.sendMessage = function sendMessage (rec) {
+  console.log(configuration)
   var user = configuration.user
   var pass = configuration.pass
   var fromEmail = configuration.fromEmail
@@ -23,19 +25,20 @@ exports.sendMessage = function sendMessage (rec, cb) {
   var subject = rec.email.subject
   var body = rec.email.body
 
-  connection.connect(function () {
-    connection.login({user: user, pass: pass}, function (err) {
-      if (err) return logger.error(err)
-      var envelope = {
-        from: fromEmail,
-        to: toEmail
-      }
-      var message = 'Subject: ' + subject + '\n\n' + body
-      connection.send(envelope, message, function (err, info) {
-        connection.quit()
-        if (cb) cb(err)
-        if (err) return logger.error(err)
-        logger.debug('Successfully sent email to %s', toEmail)
+  return new Promise(function (resolve, reject) {
+    connection.connect(function () {
+      connection.login({user: user, pass: pass}, function (err) {
+        if (err) return reject(err)
+        var envelope = {
+          from: fromEmail,
+          to: toEmail
+        }
+        var message = 'Subject: ' + subject + '\n\n' + body
+        connection.send(envelope, message, function (err, info) {
+          connection.quit()
+          if (err) return reject(err)
+          resolve()
+        })
       })
     })
   })
