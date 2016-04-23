1691cf90d1906ee453d8a7933bbea9800c5e10c5
Josh Harvey <josh@lamassu.is>
2016-04-23 22:10:35 +0300

    remove debug

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ exports.config = function config (_config) {
 }
 
 exports.sendMessage = function sendMessage (rec) {
-  console.log('DEBUG63')
-
   var user = configuration.user
   var pass = configuration.pass
   var fromEmail = configuration.fromEmail
@@ -25,28 +23,21 @@ exports.sendMessage = function sendMessage (rec) {
   var subject = rec.email.subject
   var body = rec.email.body
 
-  console.log('DEBUG64')
   var connection = new SMTPConnection(OPTIONS)
 
   return new Promise(function (resolve, reject) {
-    console.log('DEBUG65')
     connection.connect(function (err) {
       if (err) return reject(err)
-      console.log('DEBUG66')
       connection.login({user: user, pass: pass}, function (err) {
-        console.log('DEBUG67')
         if (err) return reject(err)
         var envelope = {
           from: fromEmail,
           to: toEmail
         }
         var message = 'Subject: ' + subject + '\n\n' + body
-        console.log('DEBUG60')
         connection.send(envelope, message, function (err, info) {
-          console.log('DEBUG61')
           connection.quit()
           if (err) return reject(err)
-          console.log('DEBUG62')
           resolve()
         })
       })
